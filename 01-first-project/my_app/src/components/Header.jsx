a29f0f6abf89d3346fd590b1859a95f53c901389
Alina Sevryuk <alina.sevryuk0708@gmail.com>
2022-01-29 13:39:34 +0300

    app

---
+++ 01-first-project/my_app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <header className='header'>
+        <img src="https://avatars.mds.yandex.net/i?id=36f22f2de64a0ddb5ea5fba88f90560c-5661150-images-thumbs&n=13"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ 01-first-project/my_app/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () =>{
+    return(
+        <div className='content'>
+        <div><img src='https://scheerpartners.com/wp-content/uploads/2020/05/Philadelphia-Image.jpeg'/></div>
+        <div>ava + dicription</div>
+        <div>My posts
+          <div>new post</div>
+             <div>
+               <div>post1</div>
+               <div>post2</div>
+             </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
